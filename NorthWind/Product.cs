d3ad207c5c5755a896eb7cd78e663654d84cbd7f
Alexander Ortiz <aortiz@webcreek.com>
2020-04-12 12:46:10 -0500

    Agregar archivos de proyecto.

---
+++ NorthWind/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Northwind.Models
+{
+    public class Product
+    {
+        public bool IsDiscontinued { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Id { get; set; }
+        public int SupplierId { get; set; }
+        public string Package { get; set; }
+        public string ProductName { get; set; }
+    }
+}
